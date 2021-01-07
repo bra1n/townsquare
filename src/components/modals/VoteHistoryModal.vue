be89d51ae639e8910083eab6748735eab1ba7549
Steffen <steffen@baumgart.biz>
2021-01-07 20:25:12 +0100

    added vote time

--- src/components/modals/VoteHistoryModal.vue
+++ src/components/modals/VoteHistoryModal.vue
@@ -15,30 +15,41 @@
     <table>
       <thead>
         <tr>
+          <td>Time</td>
           <td>Nominator</td>
           <td>Nominee</td>
           <td>Type</td>
           <td>Votes</td>
-          <td><font-awesome-icon icon="hand-paper" /> Hand up</td>
           <td>Majority</td>
+          <td>
+            <font-awesome-icon icon="user-friends" />
+            Voters
+          </td>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(vote, index) in session.voteHistory" :key="index">
+          <td>
+            {{ vote.timestamp.getHours() }}:{{ vote.timestamp.getMinutes() }}
+          </td>
           <td>{{ vote.nominator }}</td>
           <td>{{ vote.nominee }}</td>
           <td>{{ vote.type }}</td>
-          <td>{{ vote.votes.length }}</td>
           <td>
-            <font-awesome-icon icon="user-friends" />
-            {{ vote.votes.join(", ") }}
+            {{ vote.votes.length }}
+            <font-awesome-icon icon="hand-paper" />
           </td>
           <td>
             {{ vote.majority }}
             <font-awesome-icon
-              icon="check-square"
-              v-if="vote.votes.length >= vote.majority"
+              :icon="[
+                'fas',
+                vote.votes.length >= vote.majority ? 'check-square' : 'square'
+              ]"
             />
+          </td>
+          <td>
+            {{ vote.votes.join(", ") }}
           </td>
         </tr>
       </tbody>
@@ -96,13 +107,13 @@ thead td {
 }
 
 tbody {
-  td:nth-child(1) {
+  td:nth-child(2) {
     color: $townsfolk;
   }
-  td:nth-child(2) {
+  td:nth-child(3) {
     color: $demon;
   }
-  td:nth-child(4) {
+  td:nth-child(5) {
     text-align: center;
   }
   td:nth-child(6) {
